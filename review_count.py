2c6f0341134d9b417eb504fec1f2d73487894b66
Samson Ng <ngsamson38@gmail.com>
2019-03-16 00:43:39 +0800

    sort: data include 'good'

--- review_count.py
+++ review_count.py
@@ -15,9 +15,16 @@ for d in data:
     avg = sum_len / len(data)
 print('留言平均長度為', avg)
 
-#sort : data shorter than 100
+#sort: data shorter than 100
 new = []
 for d in data:
     if len(d) <100:
         new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
+
+#sort: data include 'good'
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(good)
+print('一共有', len(good), '筆留言提到\'good\'')
