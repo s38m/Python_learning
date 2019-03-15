2a0a678e70f3b713f3cb8b61526be7bc8b7c234a
Samson Ng <ngsamson38@gmail.com>
2019-03-15 18:21:00 +0800

    change to while chance>0

--- password_guest.py
+++ password_guest.py
@@ -2,18 +2,16 @@
 #password guest
 
 print('猜密碼遊戲')
-pw = input('請輸入密碼: ')
-test = 0
-chance = 2
+pwd = '123456'
+chance = 3
 
-while pw != '123456':
-    if test < 2:
-        print('密碼錯誤! 還有%d次機會' %chance)
-        pw = input('請輸入密碼: ')
-        test += 1
-        chance -= 1        
-    else:
-        print('遊戲失敗')
+while chance > 0:
+    data = input('請輸入密碼: ')
+    if pwd == data:
+        print('恭喜你!猜對了!')
         break
+    else:
+        chance -= 1
+        print('密碼錯誤! 剩餘%d次機會' %chance)
 else:
-    print('答對了!!')
+    print('遊戲失敗!!')
