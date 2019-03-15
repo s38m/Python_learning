184c11432643af53b7197e8e5e9d057e3d1ac9a9
Samson Ng <ngsamson38@gmail.com>
2019-03-15 18:26:03 +0800

    improve structure

--- password_guest.py
+++ password_guest.py
@@ -7,11 +7,12 @@ chance = 3
 
 while chance > 0:
     data = input('請輸入密碼: ')
+    chance -= 1
     if pwd == data:
         print('恭喜你!猜對了!')
         break
     else:
-        chance -= 1
-        print('密碼錯誤! 剩餘%d次機會' %chance)
-else:
-    print('遊戲失敗!!')
+        if chance > 0:
+            print('密碼錯誤! 剩餘%d次機會' %chance)
+        else:
+            print('遊戲失敗!!')
