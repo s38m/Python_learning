9e8c77055be297ceab5948e725d5d0a1254145a4
Samson Ng <ngsamson38@gmail.com>
2019-03-16 00:32:23 +0800

    add average length for each data

--- review_count.py
+++ review_count.py
@@ -1,6 +1,16 @@
+#encoding = utf-8
+#file i/o exercise
+
 data = []
 
 with open('review.txt', 'r') as f:
     for line in f:
         data.append(line)
-        print(len(data))
+print('檔案讀取完成，總共有', len(data), '筆資料')
+
+#average length for each data
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+    avg = sum_len / len(data)
+print('留言平均長度為', avg)
