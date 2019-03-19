5eaf302160a3259b315ed3a1b7ef8d1298513fe4
Samson Ng <ngsamson38@gmail.com>
2019-03-19 22:20:47 +0800

    shorten code

--- leaf_year.py
+++ leaf_year.py
@@ -1,12 +1,8 @@
 def is_leaf(year):
-    if year % 4 != 0:
-        return '平年'
-    elif year % 4 == 0 and year % 100 != 0:
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0 and year % 3200 != 0):
         return '潤年'
-    elif year % 100 == 0 and year % 400 != 0:
+    else:
         return '平年'
-    elif year % 400 == 0 and year % 3200 != 0:
-        return '潤年'
 
 y = int(input('請輸入年份: '))
 print(is_leaf(y))
